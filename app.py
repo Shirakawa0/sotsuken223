ee38c8bb5ed085dbc3526d928469a58af7b0d176
Shirakawa0 <t.shirakawa.sys21@morijyobi.ac.jp>
2022-09-30 12:28:36 +0900

    a

--- app.py
+++ app.py
@@ -8,22 +8,9 @@ app.secret_key = "".join(random.choices(string.ascii_letters, k=256))
 def u_login_page():
     return render_template("u_login.html")
 
-@app.route("/signup2")
-def signup2_page():
-    return render_template("signup_2.html")
-
 @app.route("/signup")
 def signup_page():
     return render_template("signup.html")
-
-@app.route("/aaa")
-def aaa_page():
-    return render_template("aaa_2.html")
-
-
-@app.route("/test")
-def test():
-    return
 
 if __name__ == "__main__":
     app.run(debug=True)
