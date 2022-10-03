9f506e0fd1ded19b63f5e1fd4c4df0c157311295
Shirakawa0 <t.shirakawa.sys21@morijyobi.ac.jp>
2022-10-03 10:06:39 +0900

    変更

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ import random, string
 app = Flask(__name__)
 app.secret_key = "".join(random.choices(string.ascii_letters, k=256))
 
-@app.route("/u_login")
+@app.route("/")
 def u_login_page():
     return render_template("u_login.html")
 
-@app.route("/u_signup_page")
+@app.route("/u_signup")
 def u_signup_page():
     return render_template("u_signup_1.html")
 
