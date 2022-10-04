eece9ba4d59facc2723fb3b407dd7f31321e7585
netannyny <y.yamazaki.sys21@morijyobi.ac.jp>
2022-10-04 12:39:10 +0900

    a

--- init.sql
+++ init.sql
@@ -7,3 +7,28 @@ CREATE TABLE u_account(
     name VARCHAR(16) NOT NULL,
     class_id VARCHAR(4) NOT NULL
 );
+
+CREATE TABLE teacher_class(
+    id varchar(64) PRIMARY KEY NOT NULL,
+    class_id VARCHAR(4) PRIMARY KEY NOT NULL
+);
+
+CREATE TABLE a_account(
+    id VARCHAR(64) PRIMARY KEY NOT NULL,
+    hash_pw VARCHAR(128) NOT NULL,
+    salt VARCHAR(5) NOT NULL,
+    name VARCHAR(16) NOT NULL,
+    public_flg BOOLEAN NOT NULL DEFAULT 0
+);
+
+CREATE TABLE comments(
+    id int NOT NULL auto_increment PRIMARY KEY,
+    thread_id int NOT NULL PRIMARY KEY,
+    body VARCHAR(500) NOT NULL
+);
+
+CREATE TABLE threads(
+    id int NOT NULL auto_increment PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
