d7e6f7460e1c546c1b2c6a12bea840603917ec15
netannyny <y.yamazaki.sys21@morijyobi.ac.jp>
2022-10-04 11:50:48 +0900

    a

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,redirect,url_for
 import random, string
 from db.db_manager import db_manager
 
@@ -8,6 +8,31 @@ app.secret_key = "".join(random.choices(string.ascii_letters, k=256))
 @app.route("/")
 def u_login_page():
     return render_template("u_login.html")
+
+@app.route("/",methods=["POST"])
+def u_login():
+    id = request.form.get("id")
+    pw = request.form.get("pw")
+    
+    dbmg = db_manager.db_manager()
+    sql = "select * from u_account where id=%s"
+    result = dbmg.exec_query(sql, id)
+
+    hash_pw, salt = dbmg.calc_pw_hash(pw, result[0]["salt"])
+
+    print("hash_pw" + hash_pw)
+    print("db_pw" + result[0]["hash_pw"])
+
+    if hash_pw == result[0]["hash_pw"]: 
+        #session["user_name"] = result[0]["name"]
+
+        # sessionの有効期限
+        #session.permanent = True
+        #app.permanent_session_lifetime = timedelta(minutes=30)
+        return render_template("u_add_1.html.html")
+    else:
+        return redirect(url_for("u_login_page")) 
+
 
 @app.route("/u_signup")
 def u_signup_page():
