1c208a417cad4c9a57d324c3633030fc6d9ff986
Shirakawa0 <t.shirakawa.sys21@morijyobi.ac.jp>
2022-10-05 11:50:31 +0900

    a

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
+from datetime import timedelta
 import random, string
 from db.db_manager import db_manager
 
@@ -9,11 +10,33 @@ app.secret_key = "".join(random.choices(string.ascii_letters, k=256))
 def u_login_page():
     return render_template("u_login.html")
 
+@app.route("/", methods=["POST"])
+def u_login():
+    id = request.form.get("id")
+    pw = request.form.get("pw")
+    
+    dbmg = db_manager()
+    sql = "select * from u_account where id=%s"
+    result = dbmg.exec_query(sql, id)
+
+    hash_pw, _ = dbmg.calc_pw_hash(pw, result[0]["salt"])
+
+    if hash_pw == result[0]["hash_pw"]:
+        session["id"] = result[0]["id"]
+        session["user_name"] = result[0]["name"]
+        # sessionの有効期限
+        session.permanent = True
+        app.permanent_session_lifetime = timedelta(minutes=30)
+        return redirect("/home")
+    else:
+        return redirect("/") 
+
+
 @app.route("/u_signup")
 def u_signup_page():
     return render_template("u_signup_1.html")
 
-@app.route("/u_signup",methods=["POST"])
+@app.route("/u_signup", methods=["POST"])
 def u_signup():
     id = request.form.get("id")
     pw = request.form.get("pw")
@@ -22,10 +45,12 @@ def u_signup():
     grade = request.form.get("grade")
     Class = request.form.get("class") # 区別のためcは大文字
 
-    
-    
-    param = (id,pw,name,dep,grade,Class)
     dbmg = db_manager()
+    hash_pw, salt = dbmg.calc_pw_hash(pw)
+
+    class_id = dep + grade + Class
+
+    dbmg.exec_query("insert into u_account values(%s,%s,%s,%s,%s)",(id,hash_pw,salt,name,class_id))
 
     return render_template("u_signup_3.html")
 
