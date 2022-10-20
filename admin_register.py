7174f20ae020d7accca5a7e68fcd49acc42e4dd3
shu0913 <s.sakamoto.sys21@morijyobi.ac.jp>
2022-10-20 12:29:45 +0900

    n

--- admin_register.py
+++ admin_register.py
@@ -2,9 +2,9 @@ from db.db_manager import db_manager
 
 dbmg = db_manager()
 
-id = "hidetoshi"
-pw = "0827"
-name = "桑田英寿"
+id = "1"
+pw = "1"
+name = "サンフランシスコ"
 
 hash_pw,salt = dbmg.calc_pw_hash(pw)
 dbmg.exec_query("insert into a_account values(%s,%s,%s,%s,%s)",(id,hash_pw,salt,name,False))
