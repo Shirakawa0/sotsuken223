d38e3fd470732ef2113ff8173a270b966594834d
netannyny <y.yamazaki.sys21@morijyobi.ac.jp>
2022-10-21 11:42:16 +0900

    aaaa

--- app.py
+++ app.py
@@ -469,6 +469,7 @@ def a_home_page():
     date = str(datetime.date.today())
     date_time_s = date + " " + "00:00:00"
     date_time_e = date + " " + "23:59:59"
+    #本日選考予定(sql変更予定)
     sql = "select u_account.name as name,schedule.company as company,schedule.step as step,schedule.detail as detail,substring(schedule.date_time,12,5) as date_time from schedule left join u_account on schedule.id = u_account.id where date_time <= %s and date_time >= %s"
     schedules = dbmg.exec_query(sql,(date_time_e,date_time_s))
     sql = "select d.name as dep,c.grade as grade,c.class as class,b.name as name,right(date,5) as date from practice a,u_account b,class c,dep d where a.student = b.id and b.class_id = c.id and c.dep_id = d.id and a.teacher = %s"
