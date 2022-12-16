5983eab5db168d189d960c2ec054703a2821f716
Shirakawa0 <t.shirakawa.sys21@morijyobi.ac.jp>
2022-12-16 11:15:56 +0900

    a

--- admin_register.py
+++ admin_register.py
@@ -2,9 +2,9 @@ from db.db_manager import db_manager
 
 dbmg = db_manager()
 
-id = "root@morijyobi.ac.jp"
+id = "test@morijyobi.ac.jp"
 pw = "password"
-name = "管理太郎"
+name = "就活太郎"
 
 hash_pw,salt = dbmg.calc_pw_hash(pw)
 dbmg.exec_query("insert into a_account values(%s,%s,%s,%s,%s)",(id,hash_pw,salt,name,False))
