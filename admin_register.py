c5eacb42014bb568976b95c1c1a6352328c2f681
Shirakawa0 <t.shirakawa.sys21@morijyobi.ac.jp>
2023-02-01 12:33:09 +0900

    a

--- admin_register.py
+++ admin_register.py
@@ -1,10 +1,28 @@
 from db.db_manager import db_manager
+from pymysql import IntegrityError
 
 dbmg = db_manager()
 
 id = "test@morijyobi.ac.jp"
 pw = "password"
 name = "就活太郎"
+# 担当クラス1（必須・4桁のクラスIDを文字列で記述）
+class1 = "2301"
+# 担当クラス2（省略可・4桁のクラスIDを文字列で記述・省略する場合は None を設定）
+class2 = "2504"
+# class2 = None
 
 hash_pw,salt = dbmg.calc_pw_hash(pw)
-dbmg.exec_query("insert into a_account values(%s,%s,%s,%s,%s)",(id,hash_pw,salt,name,False))
+
+register_flg = True
+try:
+    dbmg.exec_query("insert into a_account values(%s,%s,%s,%s,%s)",(id,hash_pw,salt,name,False))
+except IntegrityError:
+    register_flg = False
+    print("そのIDはすでに使われています")
+
+if register_flg:
+    if class1:
+        dbmg.exec_query("insert into teacher_class values(%s,%s)",(id,class1))
+    if class2:
+        dbmg.exec_query("insert into teacher_class values(%s,%s)",(id,class2))
